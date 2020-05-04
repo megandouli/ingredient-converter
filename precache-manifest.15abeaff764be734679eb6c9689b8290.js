0a6159c5f82f70e11664521733c68727ad01e544
Megan Li <meganlidou@gmail.com>
2020-05-04 19:22:36 -0400

    Updates

---
+++ precache-manifest.15abeaff764be734679eb6c9689b8290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f37a954e1e1004fb13e74de1b65c5d",
+    "url": "/ingredient-converter/index.html"
+  },
+  {
+    "revision": "5cf4557ed3ddc2df5b61",
+    "url": "/ingredient-converter/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8035431547fce4c147ad",
+    "url": "/ingredient-converter/static/css/main.83dbb025.chunk.css"
+  },
+  {
+    "revision": "5cf4557ed3ddc2df5b61",
+    "url": "/ingredient-converter/static/js/2.9d916a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ingredient-converter/static/js/2.9d916a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8035431547fce4c147ad",
+    "url": "/ingredient-converter/static/js/main.3aeab786.chunk.js"
+  },
+  {
+    "revision": "6376506476dab7f77465",
+    "url": "/ingredient-converter/static/js/runtime-main.c4f81ecf.js"
+  }
+]);
