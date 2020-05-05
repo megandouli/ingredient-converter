5953e3fb7db72cc75b8cfae90ab9cd2786caf4c9
Megan Li <meganlidou@gmail.com>
2020-05-04 22:14:04 -0400

    added responsive sizing and re-ran build

---
+++ build/precache-manifest.d49931e70d207639eaa911bc2844b965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab451e94d7e73749f737881dc2cf275",
+    "url": "/ingredient-converter/index.html"
+  },
+  {
+    "revision": "5cf4557ed3ddc2df5b61",
+    "url": "/ingredient-converter/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "71ea7e7db3b1c920a1fa",
+    "url": "/ingredient-converter/static/css/main.a2ab0887.chunk.css"
+  },
+  {
+    "revision": "5cf4557ed3ddc2df5b61",
+    "url": "/ingredient-converter/static/js/2.9d916a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ingredient-converter/static/js/2.9d916a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ea7e7db3b1c920a1fa",
+    "url": "/ingredient-converter/static/js/main.17fd4116.chunk.js"
+  },
+  {
+    "revision": "6376506476dab7f77465",
+    "url": "/ingredient-converter/static/js/runtime-main.c4f81ecf.js"
+  }
+]);
